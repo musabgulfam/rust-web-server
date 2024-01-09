7287008d81630ea4fb29b1846fe5b633f5fda435
Musab Gulfam <musabgulfam0722@gmail.com>
2024-01-09 13:04:34 +0500

    Updates

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::io::{Read, Write};
 use std::fs;
 use std::path::Path;
 use std::ffi::OsStr;
+use std::sync::Arc;
+use serde::{Serialize, Deserialize};
 
 #[get("/notes")]
 async fn get_notes() -> impl Responder {
@@ -28,17 +30,25 @@ async fn get_notes() -> impl Responder {
 //     .await
 // }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Note {
     title: String,
     note: String,
     id: u64
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct State {
     notes: Vec<Note>,
     updated_id: u64
+}
+
+#[get("/")]
+async fn index(data: web::Data<State>) -> HttpResponse {
+    let notes = &data.notes; // <- get app_name
+    println!("Notes: {:?}", notes);
+    let serialized = serde_json::to_string(&data.notes).unwrap();
+    HttpResponse::Ok().json(serialized)
 }
 
 impl State {
@@ -59,6 +69,7 @@ impl State {
                         if extension == "txt" {
                             if let Some(txt_file_name) = Path::new(path_str).file_stem().unwrap().to_str() {
                                 if let Ok(id) = txt_file_name.parse::<u64>() {
+                                    println!("ID: {:?}", id);
                                     if let Ok(contents) = fs::read_to_string(path_str) {
                                         let mut parts = contents.split('\n');
                                         if let Some(title) = parts.next() {
@@ -118,50 +129,63 @@ impl State {
 fn create_note (title: String, note: String, mut state: State) -> Result<State, std::io::Error> {
     
     // Create the state retrival mechanism here, if exists, else create a new state file
-    let mut file = match File::open("state.txt"){
-        Ok(mut file) => {
-            // state.updated_id += 1;
-            let file_name = format!("{}.txt", state.updated_id);
-            let mut file = match File::create(&file_name) {
-                Ok(file) => file,
-                Err(err) => return Err(err)
-            };
+    if let Ok(mut file) = File::open("state.txt") {
         
-            match file.write_all(format!("{}", note).as_bytes()){
-                Ok(_) => {
-                    // Create note part
-                    let new_note = Note{
-                        title: title.to_string(), 
-                        note: note.to_string(), 
-                        id: state.updated_id
-                    };
-                    state.notes.push(new_note);
-                    state.updated_id += 1;
-                    // Save the state
-                    let mut state_file = match File::create("state.txt") {
-                        Ok(mut file) => {
-                            file.write_all(format!("{}", state.updated_id).as_bytes())
-                        },
-                        Err(err) => return Err(err)
-                    };
-                    Ok(state)
-                },
-                Err(err) => return Err(err)
-            }
-        },
-        Err(err) => return Err(err)
-    };
-    file
+        // state.updated_id += 1;
+        let file_name = format!("{}.txt", state.updated_id);
+        let mut file = match File::create(&file_name) {
+            Ok(file) => file,
+            Err(err) => return Err(err)
+        };
+    
+        match file.write_all(format!("{}\n{}", title,  note).as_bytes()){
+            Ok(_) => {
+                // Create note part
+                let new_note = Note{
+                    title: title.to_string(), 
+                    note: note.to_string(), 
+                    id: state.updated_id
+                };
+                state.notes.push(new_note);
+                state.updated_id += 1;
+                // Save the state
+                let mut state_file = match File::create("state.txt") {
+                    Ok(mut file) => {
+                        file.write_all(format!("{}", state.updated_id).as_bytes())
+                    },
+                    Err(err) => return Err(err)
+                };
+                Ok(state)
+            },
+            Err(err) => return Err(err)
+        }
+    }
+    else {
+        Err(std::io::Error::new(std::io::ErrorKind::NotFound, "State file not found"))
+    }
 }
 
-fn main() {
-    let state = State::create();
-    println!("{:?}", state);
-    let title = String::from("Title");
-    let note = String::from("Hello, world!1");
-    create_note(
-        title.to_string(),
-        note.to_string(),
-        state
-    );
+// fn main() {
+//     let state = State::create();
+//     println!("{:?}", state);
+//     let title = String::from("Title");
+//     let note = String::from("Hello, world!");
+//     create_note(
+//         title.to_string(),
+//         note.to_string(),
+//         state
+//     );
+// }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let state = State::create();
+        App::new()
+            .app_data(web::Data::new(state))
+            .service(index)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
